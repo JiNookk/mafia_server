0bfc2b17a9e5e38db3a4176874b9df33a92d54b0
liam-classum <135945230+liam-classum@users.noreply.github.com>
2025-10-04 12:37:09 +0900

    add: initial setting

---
+++ src/main/java/com/jingwook/mafia_server/MafiaServerApplication.java
@@ -0,0 +1,13 @@
+package com.jingwook.mafia_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafiaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jingwook/mafia_server/MafiaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingwook.mafia_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
