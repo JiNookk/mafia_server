72baff25b2c4c312c3999fa559f8cb8812770543
liam-classum <135945230+liam-classum@users.noreply.github.com>
2025-10-11 12:10:01 +0900

    add: 포매터 추가

--- src/main/java/com/jingwook/mafia_server/services/AuthService.java
+++ src/main/java/com/jingwook/mafia_server/services/AuthService.java
@@ -1,33 +1,33 @@
 package com.jingwook.mafia_server.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jingwook.mafia_server.domains.User;
-import com.jingwook.mafia_server.dtos.SessionResponseDto;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.UUID;
+
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jingwook.mafia_server.domains.User;
+import com.jingwook.mafia_server.dtos.SessionResponseDto;
+
+import reactor.core.publisher.Mono;
 
 @Service
 public class AuthService {
     private final ReactiveRedisTemplate<String, String> redisTemplate;
     private final ObjectMapper objectMapper;
 
-
     private static final String SESSION_PREFIX = "session:";
     private static final String NICKNAME_PREFIX = "nickname:";
     private static final Duration SESSION_TTL = Duration.ofHours(1);
 
-    public AuthService(ReactiveRedisTemplate<String, String> redisTemplate, ObjectMapper objectMapper){
+    public AuthService(ReactiveRedisTemplate<String, String> redisTemplate, ObjectMapper objectMapper) {
         this.redisTemplate = redisTemplate;
         this.objectMapper = objectMapper;
     }
 
-
-    private Mono<Boolean> checkNicknameExists(String nickname){
+    private Mono<Boolean> checkNicknameExists(String nickname) {
         return redisTemplate.hasKey(NICKNAME_PREFIX + nickname);
     }
 
@@ -35,8 +35,7 @@ public class AuthService {
         return checkNicknameExists(nickname)
                 .flatMap(exists -> exists
                         ? renewUserSession(nickname)
-                        : createUserSession(nickname)
-                );
+                        : createUserSession(nickname));
     }
 
     private Mono<SessionResponseDto> renewUserSession(String nickname) {
@@ -44,31 +43,26 @@ public class AuthService {
         return redisTemplate.opsForValue()
                 .get(nickname)
                 .doOnNext(sessionId -> System.out.println(sessionId))
-                .flatMap(sessionId ->
-                        redisTemplate.expire(nickname, SESSION_TTL)
-                                .thenReturn(
-                                        new SessionResponseDto(sessionId, nickname)
-                                )
-                );
+                .flatMap(sessionId -> redisTemplate.expire(nickname, SESSION_TTL)
+                        .thenReturn(
+                                new SessionResponseDto(sessionId, nickname)));
     }
 
     private Mono<SessionResponseDto> createUserSession(String nickname) {
         String sessionId = UUID.randomUUID().toString();
         User user = new User(sessionId, nickname, LocalDateTime.now());
 
-
-        try{
+        try {
             String userJson = objectMapper.writeValueAsString(user);
 
             return redisTemplate.opsForValue()
                     .set(SESSION_PREFIX + sessionId, userJson)
                     .then(
-                            redisTemplate.expire(SESSION_PREFIX + sessionId, SESSION_TTL)
-                    )
+                            redisTemplate.expire(SESSION_PREFIX + sessionId, SESSION_TTL))
                     .then(
                             redisTemplate.opsForValue()
-                                    .set(NICKNAME_PREFIX + nickname, sessionId)
-                    ).then(redisTemplate.expire(NICKNAME_PREFIX + nickname, SESSION_TTL))
+                                    .set(NICKNAME_PREFIX + nickname, sessionId))
+                    .then(redisTemplate.expire(NICKNAME_PREFIX + nickname, SESSION_TTL))
                     .thenReturn(new SessionResponseDto(sessionId, nickname));
         } catch (Exception e) {
             throw new RuntimeException(e);
